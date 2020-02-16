241af0c96f91e85b1f2169eaa31f7c331c58476e
Thao Le <thao.le@arter.fi>
2020-02-16 14:29:04 +0200

    week3: connect to database

--- src/main/java/com/example/superbookstore/SuperbookstoreApplication.java
+++ src/main/java/com/example/superbookstore/SuperbookstoreApplication.java
@@ -4,10 +4,15 @@ import com.example.superbookstore.domain.Book;
 import com.example.superbookstore.domain.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+
 public class SuperbookstoreApplication {
 
 	public static void main(String[] args) {
