b4924ccbb692cfd4ae1d30107849cd3ff8096d1c
Thao Le <thao.le@arter.fi>
2020-02-16 20:32:32 +0200

    week3: finish week 3

--- src/main/java/com/example/superbookstore/domain/BookRepository.java
+++ src/main/java/com/example/superbookstore/domain/BookRepository.java
@@ -3,4 +3,5 @@ package com.example.superbookstore.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository  extends CrudRepository<Book, Long> {
+    Book findById(String id);
 }
--- src/main/java/com/example/superbookstore/web/BookController.java
+++ src/main/java/com/example/superbookstore/web/BookController.java
@@ -1,22 +1,59 @@
 package com.example.superbookstore.web;
 
+import com.example.superbookstore.domain.Book;
 import com.example.superbookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class BookController {
     @Autowired
     private BookRepository repository;
 
-    @RequestMapping(value= {"/", "/index"})
+    @RequestMapping(value= {"/"})
     public String BookList(Model model){
         // add attribute to html view
         model.addAttribute("books", repository.findAll());
         // render view html
-        return "books";
+        return "booklist";
     }
 
+    @RequestMapping(value = "/add")
+    public String addStudent(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public String editsSave(@ModelAttribute(value="book") Book book, @PathVariable("id") Long bookId ){
+        book.setId(bookId);
+        repository.save(book);
+        return "redirect:../";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteStudent(@PathVariable("id") Long studentId, Model model) {
+        repository.deleteById(studentId);
+        return "redirect:../";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Book foo = repository.findById(id).orElse(null);
+        model.addAttribute("book", foo);
+       /* Optional<Book> foo = repository.findById(id);
+        model.addAttribute("book", repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id)));*/
+
+        return "editbook";
+    }
 }
